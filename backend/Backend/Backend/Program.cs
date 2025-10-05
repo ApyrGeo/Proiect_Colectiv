353906730c9b8f92627d47db5a524fe5121d6dea
George Secure <geosebi2004@gmail.com>
2025-10-05 21:13:22 +0300

    Implemented Logging System

--- backend/Backend/Backend/Program.cs
+++ backend/Backend/Backend/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Logging.ClearProviders();
+builder.Logging.AddLog4Net("log4net.config");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ backend/Backend/Backend/Utils/LogProvider.cs
@@ -0,0 +1,13 @@
+﻿using log4net;
+
+namespace Backend.Utils;
+
+public static class LogProvider
+{
+    private static readonly ILog _log = LogManager.GetLogger(typeof(LogProvider));
+
+    public static ILog GetLogger()
+    {
+        return _log;
+    }
+}
