c367cc384d783931b9b0937126b9d473e181b6ed
Andrei0921 <rotaruandrei904@gmail.com>
2025-10-31 20:48:51 +0200

    added controller

--- backend/Backend/Backend/Controllers/TimetableController.cs
+++ backend/Backend/Backend/Controllers/TimetableController.cs
@@ -1,4 +1,5 @@
-﻿using Backend.Interfaces;
+﻿using Backend.Domain.DTOs;
+using Backend.Interfaces;
 using log4net;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,29 @@ public class TimetableController(ITimetableService service) : ControllerBase
 {
     private readonly ILog _logger = LogManager.GetLogger(typeof(AcademicsController));
     private readonly ITimetableService _service = service;
+    
+    [HttpGet("subjects/{subjectId}")]
+    [ProducesResponseType(200)]
+    public async Task<ActionResult<SubjectResponseDTO>> GetSubjectById([FromRoute] int subjectId)
+    {
+        _logger.InfoFormat("Fetching subject with id {0}", subjectId);
+
+        SubjectResponseDTO subjects = await _service.GetSubjectById(subjectId);
+
+        return Ok(subjects);
+    }
+
+    [HttpPost("subjects")]
+    [ProducesResponseType(201)]
+    [ProducesResponseType(422)]
+    public async Task<ActionResult<FacultyResponseDTO>> CreateSubject([FromBody] SubjectPostDTO subjectPostDto)
+    {
+        _logger.InfoFormat("Creating new faculty with name {0}", subjectPostDto.Name);
+
+        SubjectResponseDTO createdSubject = await _service.CreateSubject(subjectPostDto);
+
+        return CreatedAtAction(nameof(GetSubjectById), new { subjectId = createdSubject.Id }, createdSubject);
+    }
     
     
     
