cfede63b817fefc2c1125fa25bbfc3b265495f3e
Andrei0921 <rotaruandrei904@gmail.com>
2025-11-05 21:27:09 +0200

    added EmailProvider interface

---
+++ backend/Backend/EmailService/Interfaces/IEmailProvider.cs
@@ -0,0 +1,9 @@
+﻿using EmailService.Models;
+
+namespace EmailService.Interfaces;
+
+public interface IEmailProvider
+{
+    Task SendCreateAccountEmailAsync(string to, CreatedUserModel model);
+    Task SendCreateEnrollmentEmailAsync(string to, CreatedEnrollmentModel model);
+}
