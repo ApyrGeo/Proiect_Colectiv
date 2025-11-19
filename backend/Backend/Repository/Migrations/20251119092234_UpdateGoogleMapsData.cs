1dd1358aa8a1430abf815cc9bea6709a85d13cbd
TudorStanca <vlad.stanca@stud.ubbcluj.ro>
2025-11-19 11:25:58 +0200

    Refactored backend into class libraries, updated nuget packages

---
+++ backend/Backend/Repository/Migrations/20251119092234_UpdateGoogleMapsData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGoogleMapsData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GoogleMapsData_Id",
+                table: "Locations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GoogleMapsData_Id",
+                table: "Locations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
