216d04647767071b8675a7721e4e25aef6fac019
TudorStanca <vlad.stanca@stud.ubbcluj.ro>
2025-12-02 19:14:55 +0200

    Created migration for user signature

---
+++ backend/Backend/Repository/Migrations/20251202171348_AddUserSignature.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackForUBB.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSignature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Signature",
+                table: "Users",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Signature",
+                table: "Users");
+        }
+    }
+}
