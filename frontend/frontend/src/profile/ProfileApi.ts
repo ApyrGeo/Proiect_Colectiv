936bb7c895c0cc34351f28ba1cb3c401bc4beb85
Razvan <negrea1razvan@yahoo.com>
2025-12-08 14:38:05 +0200

    Fix the gap in ProfileApi.ts

--- frontend/frontend/src/profile/ProfileApi.ts
+++ frontend/frontend/src/profile/ProfileApi.ts
@@ -6,9 +6,6 @@ const ProfileApi = () => {
   const { axios } = useApiClient();
 
   const profileUrl = "/api/User";
-
-
-
   const getProfile = useCallback(async () => {
     const response = await axios.get<HourPropsDto>(`${profileUrl}/hours`);
     return response.data;
