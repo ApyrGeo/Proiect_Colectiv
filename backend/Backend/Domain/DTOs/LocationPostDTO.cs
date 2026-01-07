9332533db93999685d4fda621dc49cbd3b49b23b
Andrei0921 <rotaruandrei904@gmail.com>
2026-01-07 17:35:59 +0200

    changend LocationPostDTO.cs

--- backend/Backend/Domain/DTOs/LocationPostDTO.cs
+++ backend/Backend/Domain/DTOs/LocationPostDTO.cs
@@ -4,4 +4,6 @@ public class LocationPostDTO
 {
     public string? Name { get; set; }
     public string? Address { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
