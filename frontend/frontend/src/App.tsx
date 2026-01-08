7ed947a7eee47bedd0968b06e48b7c55c48c18ee
George Secure <geosebi2004@gmail.com>
2026-01-08 21:28:04 +0200

    Changed Desing to Admin Add Location Page + Error Handling Added Carousel component for news on Homescreen Added i18n for all fields + ability to have news in different languages

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -45,7 +45,7 @@ const App = () => {
             <Route path={"/profile"} Component={ProfilePage} />
             <Route path={"/exam"} Component={ExamPage} />
             <Route path={"/timetable-generation"} Component={TimetableGenerationPage}></Route>
-            <Route path={"/admin//location"} Component={AdminLocationPage} />
+            <Route path={"/admin/location"} Component={AdminLocationPage} />
           </Route>
         </Routes>
 
