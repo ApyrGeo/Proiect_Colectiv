ca38a0749587da07a5a8150c9f6a67cc550ae859
Andrei Horodniceanu <a.horodniceanu@proton.me>
2026-01-17 21:02:07 +0200

    Revert "Backend: Add PrettyName to Promotion Response"
    This reverts commit a88a27bf202577a6c874027e52db462ded9b114e.

--- backend/Backend/Domain/DTOs/PromotionResponseDTO.cs
+++ backend/Backend/Domain/DTOs/PromotionResponseDTO.cs
@@ -6,10 +6,7 @@ public class PromotionResponseDTO
     public required int StartYear { get; set; }
     public required int EndYear { get; set; }
 
-    public required string PrettyName { get; set; }
-
-
-    public List<StudentGroupResponseDTO> StudentGroups { get; set; } = [];
+	public List<StudentGroupResponseDTO> StudentGroups { get; set; } = [];
     
     public List<PromotionSemesterResponseDTO> Semesters { get; set; } = [];
 }
